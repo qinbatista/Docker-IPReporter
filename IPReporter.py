646dfecc43caa9d417658ffe06be2a6b9e7acdec
qin.batista <qin.batista@outlook.com>
2023-08-01 16:47:35 -0700

    update

--- IPReporter.py
+++ IPReporter.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import platform
 import socket
 
+
 class DDNSClient:
     def __init__(self, google_username, google_password, client_domain_name, server_domain_name):
         # configuration for different reporter from google domain
@@ -19,7 +20,8 @@ class DDNSClient:
         self.__target_server = server_domain_name
         self.__file_path = "/root/logs.txt"
         print(f"google_username={google_username},google_password={google_password},client_domain_name={client_domain_name}, server_domain_name={server_domain_name}")
-        if platform.system() == 'Darwin': self.__file_path = "/Users/qin/Desktop/logs.txt"
+        if platform.system() == 'Darwin':
+            self.__file_path = "/Users/qin/Desktop/logs.txt"
 
         # https://domains.google.com/checkip banned by Chinese GFW
         self._get_ip_website = "https://checkip.amazonaws.com"
@@ -61,9 +63,12 @@ class DDNSClient:
             try:
                 self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]IP: {self.__target_server}")
                 self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]IP: {(self.__target_server)}")
-                this_docker_ip = self.__get_host_ip()
-                udpClient.sendto((f"{gethostbyname(self.__target_server)},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}").encode(encoding="utf-8"), (self.__target_server, 7171))
-                self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]Updated to server={gethostbyname(self.__target_server)}, {self.__target_server}: reachable={self._can_connect} message: {this_docker_ip},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}")
+                this_docker_ipv4 = self.__get_host_ip()
+                this_docker_ipv6 = self.__get_current_ipv6()
+                print(f"this_docker_ipv4={this_docker_ipv4},this_docker_ipv6={this_docker_ipv6}")
+                udpClient.sendto((f"{gethostbyname(self.__target_server)},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}").encode(
+                    encoding="utf-8"), (self.__target_server, 7171))
+                self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]Updated to server={gethostbyname(self.__target_server)}, {self.__target_server}: reachable={self._can_connect} message: {this_docker_ipv6},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}")
                 time.sleep(60)
             except Exception as e:
                 time.sleep(60)
@@ -85,8 +90,15 @@ class DDNSClient:
             self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][get_host_ip]Error: {str(e)}")
         return self.__ip
 
+    def __get_current_ipv6(self):
+        """Get the current external IPv6 address or return None if no connection to the IPify service is possible"""
+        try:
+            return requests.get("https://api6.ipify.org", timeout=5).text
+        except requests.exceptions.ConnectionError as ex:
+            return None
+
+
 if __name__ == '__main__':
-    print(socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET6)[1][4][0])
     google_username = os.environ["GOOGLE_USERNAME"]
     google_password = os.environ["GOOGLE_PASSWORD"]
     client_domain_name = os.environ["CLIENT_DOMAIN_NAME"]
