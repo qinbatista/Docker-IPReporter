e2dd926343230a5a77ea7ed3bf132bdfb19f202e
qin.batista <qin.batista@outlook.com>
2023-08-01 15:31:39 -0700

    update

--- IPReporter.py
+++ IPReporter.py
@@ -8,16 +8,17 @@ import uuid
 import subprocess
 from socket import *
 from datetime import datetime
+import sys
 
 
 class DDNSClient:
-    def __init__(self):
-        #configuration for different reporter from google domain
-        self.__google_key = "XXXXXX"
-        self.__google_secret = "XXXXXXX"
+    def __init__(self, google_username, google_password, client_domain_name, server_domain_name):
+        # configuration for different reporter from google domain
+        self.__google_username = google_username
+        self.__google_password = google_password
         self._my_domain = "client.example.com"
 
-        #system variables
+        # system variables
         # https://domains.google.com/checkip banned by Chinese GFW
         self.__file_path = "/root/logs.txt"
         # self.__file_path = "/Users/qin/Desktop/logs.txt"
@@ -61,8 +62,9 @@ class DDNSClient:
         while True:
             try:
                 this_docker_ip = self.__get_host_ip()
-                udpClient.sendto((f"{gethostbyname(self.__target_server)},{str(self._can_connect)},{self.__google_key}:{self.__google_secret},{self._my_domain}").encode(encoding="utf-8"), (self.__target_server, 7171))
-                self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]Updated to server={gethostbyname(self.__target_server)}, {self.__target_server}: reachable={self._can_connect} message: {this_docker_ip},{str(self._can_connect)},{self.__google_key}:{self.__google_secret},{self._my_domain}")
+                udpClient.sendto((f"{gethostbyname(self.__target_server)},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}").encode(
+                    encoding="utf-8"), (self.__target_server, 7171))
+                self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]Updated to server={gethostbyname(self.__target_server)}, {self.__target_server}: reachable={self._can_connect} message: {this_docker_ip},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}")
                 time.sleep(60)
             except Exception as e:
                 self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]Error: {str(e)}")
@@ -85,7 +87,12 @@ class DDNSClient:
 
 
 if __name__ == '__main__':
-    # os.system("ssr start")
-    ss = DDNSClient()
+    if len(sys.argv) >= 4:
+        google_username = sys.argv[0]
+        google_password = sys.argv[1]
+        client_domain_name = sys.argv[2]
+        server_domain_name = sys.argv[3]
+
+    ss = DDNSClient(google_username, google_password, client_domain_name, server_domain_name)
     ss._ping_server_thread()
     ss._update_this_server_thread()
--- dockerfile
+++ dockerfile
@@ -1,8 +1,17 @@
 FROM python:3-slim
+
+
+
+ARG google_username
+ARG google_password
+ARG client_domain_name
+ARG server_domain_name
+
+
 ADD * /
 RUN apt-get update
 RUN apt-get -y install iputils-ping
 RUN pip3 install -r requirements.txt
 
 WORKDIR /root
-CMD ["python3","/IPReporter.py"]
+CMD ["python3","/IPReporter.py",google_username,google_password,client_domain_name,server_domain_name]
