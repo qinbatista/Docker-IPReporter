0feefdc54a87707acd0cc1e25702d26ddbf98372
qin.batista <qin.batista@outlook.com>
2024-01-22 15:19:17 -0800

    update

--- IPReporter.py
+++ IPReporter.py
@@ -14,7 +14,8 @@ class DDNSClient:
         self.__google_username = google_username
         self.__google_password = google_password
         self._my_domain = client_domain_name
-        self.__target_server = server_domain_name
+        self.__target_server_v4 = server_domain_name
+        self.__target_server_v6 = server_domain_name
         self.__file_path = "/ipreporter.txt"
 
         # https://domains.google.com/checkip banned by Chinese GFW
@@ -32,7 +33,7 @@ class DDNSClient:
         while True:
             try:
                 # Ping the target server
-                process = subprocess.Popen(f"ping -c 1 {self.__target_server}", stdout=subprocess.PIPE, universal_newlines=True, shell=True)
+                process = subprocess.Popen(f"ping -c 1 {self.__target_server_v4}", stdout=subprocess.PIPE, universal_newlines=True, shell=True)
                 process.wait()
 
                 # Check the result of the ping command
@@ -40,7 +41,7 @@ class DDNSClient:
                     self._can_connect = 1# can reach the server
                 else:
                     self._can_connect = 0# cannot reach the server
-                self.__log(f"[{datetime.now().strftime('^%Y-%m-%d %H:%M:%S')}][ping_server][ping -c 1 {self.__target_server}]{self._can_connect}")
+                self.__log(f"[{datetime.now().strftime('^%Y-%m-%d %H:%M:%S')}][ping_server][ping -c 1 {self.__target_server_v4}]{self._can_connect}")
 
                 # Wait for 1 minute before pinging again
                 time.sleep(60)
@@ -57,8 +58,9 @@ class DDNSClient:
             try:
                 this_docker_ipv4 = self.__get_current_ipv4()
                 this_docker_ipv6 = self.__get_current_ipv6()
-                udpClient.sendto((f"{gethostbyname(self.__target_server)},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}").encode(encoding="utf-8"), (self.__target_server, 7171))
-                self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]Updated to server={gethostbyname(self.__target_server)}, {self.__target_server}: reachable={self._can_connect} message: {this_docker_ipv4},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}")
+                udpClient.sendto((f"{gethostbyname(self.__target_server_v4)},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain},v4").encode(encoding="utf-8"), (self.__target_server_v4, 7171))
+                # udpClient.sendto((f"{gethostbyname(self.__target_server_v4)},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain},v6").encode(encoding="utf-8"), (self.__target_server_v6, 7171))
+                self.__log(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][update_this_server]Updated to server={gethostbyname(self.__target_server_v4)}, {self.__target_server_v4}: reachable={self._can_connect} message: {this_docker_ipv4},{str(self._can_connect)},{self.__google_username}:{self.__google_password},{self._my_domain}")
                 time.sleep(60)
             except Exception as e:
                 time.sleep(60)
